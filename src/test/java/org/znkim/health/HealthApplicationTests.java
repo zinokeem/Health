d1265acc40e23d38f4e994908d0196542714b429
player <player@znkim-PC>
2020-11-21 21:24:58 +0900

    first

---
+++ src/test/java/org/znkim/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.znkim.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
