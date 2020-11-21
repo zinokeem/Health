54727ab65f55e1679190c2e3e9e7e896653b14fb
znkim <gbmin14@naver.com>
2020-11-21 22:28:22 +0900

    defaultSetting

---
+++ src/main/java/org/znkim/health/MvcConfig.java
@@ -0,0 +1,7 @@
+package org.znkim.health;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer{
+
+}
---
+++ src/main/java/org/znkim/health/controller/fortest.java
@@ -0,0 +1,14 @@
+package org.znkim.health.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class fortest {	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
