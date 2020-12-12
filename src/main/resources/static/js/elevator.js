8ca0285510aa950de25152350826f8883d618a38
znkim <gbmin14@naver.com>
2020-12-12 16:46:45 +0900

    elevator javascript fixed

--- src/main/resources/static/js/elevator.js
+++ src/main/resources/static/js/elevator.js
@@ -22,16 +22,20 @@ function eventHandler() {
 	
 	$('div#buttons input.excute').click(function() {
 		$.ajax({
-			url: "/test", // 클라이언트가 요청을 보낼 서버의 URL 주소
-		    data: "", // HTTP 요청과 함께 서버로 보낼 데이터
-		    type: "GET", // HTTP 요청 방식(GET, POST)
-		    //dataType: "json"
+			url: "/test",
+		    type: "GET", 
 			dataType: "text"
 		}).done(function(result) {
 			console.log("성공");
 		}).fail(function(result) {
 			console.log("실패");
 		});
+	});
+	
+	$('div#buttons input.floor').change(function(e) {
+		var target = $(this);
+		var value = target.val();
+		console.log(value);
 	});
 }
 
