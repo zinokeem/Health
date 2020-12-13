93fe8e40fcdc73163826e569ca0d3ae42b951a41
82107 <82107@211.209.230.94>
2020-12-13 18:07:10 +0900

    Person class fixed

--- src/main/java/org/znkim/health/biz/Person.java
+++ src/main/java/org/znkim/health/biz/Person.java
@@ -1,9 +1,16 @@
 package org.znkim.health.biz;
 
 public class Person {
-	int perposeFloor;
-	int currentFloor;
-	
+	private int perposeFloor;
+	private int currentFloor;
+	private int status;		// 1:up, -1: down, 0:그자리그대로
+	public Person(int perposeFloor, int currentFloor) {
+		this.perposeFloor = perposeFloor;
+		this.currentFloor = currentFloor;
+		if(perposeFloor>currentFloor) this.status = 1;
+		else if(perposeFloor<currentFloor) this.status =-1;
+		else this.status =0;
+	}
 	public boolean compare(int elevatorFloor) {
 		return true;
 	}
