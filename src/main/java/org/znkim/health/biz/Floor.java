a4f4a1d08bf6f5e3a2124c6990c857a760fc993f
82107 <82107@211.209.230.94>
2020-12-14 20:27:51 +0900

    add Constructor Overriding

--- src/main/java/org/znkim/health/biz/Floor.java
+++ src/main/java/org/znkim/health/biz/Floor.java
@@ -2,13 +2,18 @@ package org.znkim.health.biz;
 
 import java.util.ArrayList;
 
+import org.znkim.health.model.ElevatorModel;
+
 public class Floor {
 	boolean up, down; // 현재 층의 올라감 버튼, 내림 활성화 상태
 	ArrayList<Person> waitPeoples; // 기다리고 있는 사람들
-
+	ArrayList<Person> insideTheElevator;  // 엘레베이터에 탑승한 사람들
 	public Floor(ArrayList<Person> waitPeoples, boolean up, boolean down) {
 		this.up = up;
 		this.down = down;
 		this.waitPeoples = waitPeoples;
+	}
+	public Floor(ArrayList<Person> insideTheElevator) {
+		this.insideTheElevator = insideTheElevator;
 	}
 }
