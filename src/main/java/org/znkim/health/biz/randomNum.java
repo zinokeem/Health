a3daadf2e8cd70f2844bdf638b352877a6241b78
82107 <82107@211.209.230.94>
2020-12-15 19:37:00 +0900

    creat randomNum class

---
+++ src/main/java/org/znkim/health/biz/randomNum.java
@@ -0,0 +1,17 @@
+package org.znkim.health.biz;
+import java.util.*;
+public class randomNum{
+	public int perposeFloorNum(int countFloor) {
+		Random random1 = new Random();
+		return random1.nextInt(countFloor)+1;
+	 
+	}
+	public int waitFloorNum(int countFloor) {
+		Random random2 = new Random();
+		return random2.nextInt(countFloor)+1;
+	}
+	public int personAssignment(int countFloor) {
+		Random random3 = new Random();
+		return random3.nextInt(countFloor*(30/100))+1;
+	}
+}
